aada59bfe9ba884d4b199c77ee887c436768c237
dmazui <dmazui@gmail.com>
2018-11-13 17:33:12 -0200

    ajustes parametros

--- src/main/java/br/com/projetoAsp/Main.java
+++ src/main/java/br/com/projetoAsp/Main.java
@@ -7,11 +7,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Ocr.setUp();
-		String fileName = "C:/ocrDi/testeEng.png";
+		
+		String fileName	= "C:/ocrDi/testePor.png";
+		String lang		= "por";
+		
 		Ocr ocr = new Ocr();
-		ocr.startEngine("eng", Ocr.SPEED_FASTEST); // English
+		ocr.startEngine(lang, Ocr.SPEED_SLOW); // Portugues
 		String s = ocr.recognize(new File[] {new File(fileName)},
-		  Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT); // PLAINTEXT | XML | PDF | RTF
+		  Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT); // PLAINTEXT | XML | PDF | RTF
 		System.out.println("Result: " + s);
 		ocr.stopEngine();
 	}
